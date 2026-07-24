2c1813900327e2fcefb938ca11fb55e7f23eb65d
deanmasc <deanmascitti@gmail.com>
2026-07-24 16:08:55 +1000

    Refactor SPSCQueue: improve push/get methods, enhance atomic operations, and add print functionality

--- src/spsc-queue/spsc_queue.h
+++ src/spsc-queue/spsc_queue.h
@@ -3,66 +3,101 @@
 
 #include <atomic>
 #include <array>
-#include <cstdlib>
+#include <cstdint>
 #include <optional>
 #include <iostream>
+#include <new>
+#ifndef __cpp_lib_hardware_interference_size
+    constexpr std::size_t hardware_destructive_interference_size = 64;
+#else
+    using std::hardware_destructive_interference_size;
+#endif
 
 template<typename T, uint32_t C>
 class SPSCQueue {
     static_assert(C != 0 && (C & (C - 1)) == 0, "Capacity must be a power of 2.");
 
-    std::array<T, C> ring_buffer;
-    // Both atomic to disallow race conditions
-    std::atomic<uint32_t> head {};
-    std::atomic<uint32_t> tail {};
+    alignas(hardware_destructive_interference_size) std::array<T, C> ring_buffer;
+    alignas(hardware_destructive_interference_size) std::atomic<uint32_t> head {};
+    alignas(hardware_destructive_interference_size) std::atomic<uint32_t> tail {};
 
-    uint32_t get_index(const uint32_t& idx) const {
+    uint32_t get_index(const uint32_t idx) const {
         // Use bit-mask with AND to get the remainder (C is always a power of 2)
         return idx & (C - 1); 
     }
 
-public:
-    bool push(T& val) {
-        if (is_full()) return false;
+    uint32_t size(const uint32_t head, const uint32_t tail) const {
+        return tail - head;
+    }
 
-        // This lines loads the head using memory_order_acquire, because we want to make sure the lines 
-        // after it do not run before it, so we guarentee we get tail first, then increment it
-        auto tail_idx {get_index(tail.load(std::memory_order_acquire))};
-        ring_buffer[tail_idx] = val;
-        // This line ensures that the writing of tail is indivisibleno line before the store is moved to 
+    bool is_empty(const uint32_t head, const uint32_t tail) const {
+        return this->size(head, tail) == 0;
+    }
+
+    bool is_full(const uint32_t head, const uint32_t tail) const {
+        return this->size(head, tail) == C;
+    }
+
+public:
+    bool push(const T& val) {
+        // We need to acquire the head first to ensure that any of the ahead operations
+        // read stale values, the acquire also ensures that we read the updated state from the 
+        // consumer, and that uses of this curr_head get the correct state
+        auto curr_head {head.load(std::memory_order_acquire)};
+        // We also load curr_tail here to get a copy before we update it again
+        auto curr_tail {tail.load(std::memory_order_relaxed)};
+
+        // pass into is_full so we ensure we do not need to re-acquire
+        if (is_full(curr_head, curr_tail)) return false;
+
+        ring_buffer[get_index(curr_tail)] = val;
+        // This line ensures that the writing of tail is indivisible, no line before the store is moved to 
         // after it, therefore once tail has been updated we know the operations before it have also ran
-        tail.store(++tail, std::memory_order_release);
+        // we are synchronising so that when the consumer acquired tail it will get the updated version
+        tail.store(curr_tail + 1, std::memory_order_release);
         return true;
     }
 
     bool push(T&& val) {
-        if (is_full()) return false;
+        auto curr_head {head.load(std::memory_order_acquire)};
+        auto curr_tail {tail.load(std::memory_order_relaxed)};
 
-        // This lines loads the head using memory_order_acquire, because we want to make sure the lines 
-        // after it do not run before it, so we guarentee we get tail first, then increment it
-        auto tail_idx {get_index(tail.load(std::memory_order_acquire))};
-        ring_buffer[tail_idx] = std::move(val);
-        // This line ensures that the writing of tail is indivisibleno line before the store is moved to 
+        if (is_full(curr_head, curr_tail)) return false;
+
+        ring_buffer[get_index(curr_tail)] = std::move(val);
+        // This line ensures that the writing of tail is indivisible, no line before the store is moved to 
         // after it, therefore once tail has been updated we know the operations before it have also ran
-        tail.store(++tail, std::memory_order_release);
+        tail.store(curr_tail + 1, std::memory_order_release);
         return true;
     }
     
     std::optional<T> get() {
-        if (is_empty()) return std::nullopt;
+        auto curr_head {head.load(std::memory_order_relaxed)};
+        auto curr_tail {tail.load(std::memory_order_acquire)};
 
-        // This lines loads the head using memory_order_acquire, because we want to make sure the lines 
-        // after it do not run before it, so we guarentee we get head first, then increment it
-        auto head_idx {get_index(head.load(std::memory_order_acquire))};
-        auto result{ring_buffer[head_idx]};
+        if (is_empty(curr_head, curr_tail)) return std::nullopt;
+
+        auto result = std::move(ring_buffer[get_index(curr_head)]);
         // This line ensures that the writing of tail is indivisibleno line before the store is moved to 
         // after it, therefore once tail has been updated we know the operations before it have also ran
-        head.store(++head, std::memory_order_release);
+        head.store(curr_head + 1, std::memory_order_release);
         return result;
     }
 
+    void print() const {
+        auto valid_head {head.load(std::memory_order_acquire)};
+        auto valid_tail {tail.load(std::memory_order_acquire)};
+
+        for (uint32_t i{}; i < size(valid_head, valid_tail); i++) {
+            std::cout << ring_buffer[get_index(i + valid_head)] << ", ";
+        }
+        std::cout << "\n";
+    }
+
     uint32_t size() const {
-        return tail.load(std::memory_order_acquire) - head.load(std::memory_order_acquire);
+        auto curr_head {head.load(std::memory_order_acquire)};
+        auto curr_tail {tail.load(std::memory_order_acquire)};
+        return curr_tail - curr_head;
     }
 
     bool is_empty() const {
@@ -70,17 +105,7 @@ public:
     }
 
     bool is_full() const {
-        return this->size() >= C;
-    }
-
-    void print() const {
-        auto valid_head {head.load(std::memory_order_acquire)};
-        auto valid_tail {tail.load(std::memory_order_acquire)};
-
-        for (uint32_t i{valid_head}; i < valid_tail; i++) {
-            std::cout << ring_buffer[get_index(i)] << ", ";
-        }
-        std::cout << "\n";
+        return this->size() == C;
     }
 };
 
