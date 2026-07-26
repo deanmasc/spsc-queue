a0b44e4e8812839d4d8602fec6bb902abe83acdc
deanmasc <deanmascitti@gmail.com>
2026-07-27 01:49:48 +1000

    Remove example usage of SPSCQueue from main.cpp: clean up code and simplify entry point

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,6 @@
 #include <iostream>
 
 int main() {
-    SPSCQueue<int, 4> q;
-    int x {2};
-    q.push(1);
-    q.push(x);
-    q.push(4);
-
-    q.print();
-
-    std::cout << *q.get() << std::endl;
-    q.print();
-    std::cout << *q.get() << std::endl;
-    q.print();
 
     return 0;
 }
